0f132ec0e3b9f225b76fb743e97f683623a24574
Gabriel Ivanica <ivanicagabriel@gmail.com>
2012-10-28 02:13:50 +0300

    + template

--- trivia_src/main.py
+++ trivia_src/main.py
@@ -15,6 +15,11 @@
 # limitations under the License.
 #
 import webapp2
+import jinja2
+import os
+
+jinja_environment = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 from sets import Set
 
@@ -31,18 +36,23 @@ class MainPage(webapp2.RequestHandler):
         
         user = users.get_current_user()
         
-        self.response.out.write('Bazinga!')
-        
         if user:
             logged_user = User(author = user.nickname(),
                                email  = 'some_email')
             logged_user.put()
             
-            self.response.headers['Content-Type'] = 'text/plain'
-            self.response.out.write('Hello, ' + user.nickname())
+            self.response.headers['Content-Type'] = 'text/html'
+            user_id = user.nickname()
         else:
             self.redirect(users.create_login_url(self.request.uri))
 
+        template_values = {
+            'user_id': user_id,
+        }			
+			
+        template = jinja_environment.get_template('index.html')
+        self.response.out.write(template.render(template_values))			
+			
 
 app = webapp2.WSGIApplication([('/', MainPage)],
                               debug=True)
